fd6e87df8f320dc654667372eddc93c3bc048328
diego.dona <diego.dona@outlook.com.br>
2016-11-06 11:09:34 -0200

    adjusts

--- DDona.KrunkDS.WebApp/app/pages/user/user.controller.js
+++ DDona.KrunkDS.WebApp/app/pages/user/user.controller.js
@@ -4,13 +4,16 @@
         .module('app')
         .controller('UserController', UserController);
 
-    UserController.$inject = [];
+    UserController.$inject = ['DTOptionsBuilder', 'DTColumnDefBuilder'];
 
-    function UserController() {
+    function UserController(DTOptionsBuilder, DTColumnDefBuilder) {
         var vm = this;
         vm.working = false;
         vm.tabIndex = 0;
         vm.showDetails = false;
+
+        vm.dtOptions = dtOptions();
+        vm.dtColumnDefs = dtColumnDefs();
 
         //////////////////////////////////////////////////////////////////////////
 
@@ -24,6 +27,18 @@
 
         //////////////////////////////////////////////////////////////////////////
 
+
+        //////////////////////////////////////////////////////////////////////////
+
+        function dtOptions() {
+            return DTOptionsBuilder.newOptions()
+                .withPaginationType('full_numbers')
+                .withDOM('lrtip');
+        }
+
+        function dtColumnDefs() {
+            return [];
+        }
     }
 
 }());
