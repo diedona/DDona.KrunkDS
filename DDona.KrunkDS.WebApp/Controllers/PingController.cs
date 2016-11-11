c4e0cbc8f2a3fd15a3bb8c9b34b150ba22988dfc
diego.dona <diego.dona@outlook.com.br>
2016-11-11 18:26:03 -0200

    commit

---
+++ DDona.KrunkDS.WebApp/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DDona.KrunkDS.WebApp.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Ping")]
+    public class PingController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(true);
+        }
+    }
+}
