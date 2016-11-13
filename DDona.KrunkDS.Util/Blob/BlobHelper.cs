a5fd39578a083c0b87741393c68b3f08614df454
diego.dona <diego.dona@outlook.com.br>
2016-11-13 15:32:00 -0200

    starting azure cloud blob

---
+++ DDona.KrunkDS.Util/Blob/BlobHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace DDona.KrunkDS.Util.Blob
+{
+    public class BlobHelper
+    {
+        private CloudStorageAccount _storageAccount;
+        private CloudBlobClient _blobClient;
+        private CloudBlobContainer _container;
+
+        public BlobHelper()
+        {
+            _storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            _blobClient = _storageAccount.CreateCloudBlobClient();
+            _container = _blobClient.GetContainerReference("krunkds");
+        }
+    }
+}
