b15e1f4a7ea40973e4d07d0172369c54c9277ffc
diego.dona <diego.dona@outlook.com.br>
2016-11-13 15:47:44 -0200

    upload blob

--- DDona.KrunkDS.Util/Blob/BlobHelper.cs
+++ DDona.KrunkDS.Util/Blob/BlobHelper.cs
@@ -6,20 +6,40 @@ using System.Threading.Tasks;
 using Microsoft.Azure;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
+using System.IO;
 
 namespace DDona.KrunkDS.Util.Blob
 {
+    /// <summary>
+    /// <seealso cref="https://azure.microsoft.com/pt-br/documentation/articles/storage-dotnet-how-to-use-blobs/"/>
+    /// </summary>
     public class BlobHelper
     {
         private CloudStorageAccount _storageAccount;
         private CloudBlobClient _blobClient;
         private CloudBlobContainer _container;
 
+        public List<string> Errors { get; set; }
+
         public BlobHelper()
         {
             _storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
             _blobClient = _storageAccount.CreateCloudBlobClient();
             _container = _blobClient.GetContainerReference("krunkds");
+        }
+
+        public bool UploadBlob(byte[] File, string FileName)
+        {
+            CloudBlockBlob blockBlob = _container.GetBlockBlobReference(FileName);
+            try
+            {
+                blockBlob.UploadFromByteArray(File, 0, File.Length);
+                return true;
+            } catch(Exception ex)
+            {
+                Errors.Add(ex.Message);
+                return false;
+            }
         }
     }
 }
